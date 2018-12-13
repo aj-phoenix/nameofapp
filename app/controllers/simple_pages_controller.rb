b1122947dd0556a211482092393579dd025c8c4b
A.J. Phoenix <ajphoenix8@gmail.com>
2018-12-13 18:25:56 -0500

    5.4.1 troubleshooting error

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,12 +2,6 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
-  def thank_you
-   @name = params[:name]
-   @email = params[:email]
-   @message = params[:message]
-   UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
   ActionMailer::Base.mail(from: @email,
     to: 'your-email@example.com',
     subject: "A new contact form message from #{@name}",
