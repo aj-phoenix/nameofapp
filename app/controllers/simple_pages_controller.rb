1f2a7fa10fe5a849624a49b9556e9c4c92ffd65a
A.J. Phoenix <ajphoenix8@gmail.com>
2018-12-14 21:30:48 -0500

    5.4.2 fixed error message, added sign in/sign up routs

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,15 @@
 class SimplePagesController < ApplicationController
-  def landing_page
-    @products = Product.limit(3)
-  end
-  ActionMailer::Base.mail(from: @email,
+ def landing_page
+   @products = Product.limit(3)
+ end
+ def thank_you
+   @name = params[:name]
+   @email = params[:email]
+   @message = params[:message]
+   UserMailer.contact_form(@email, @name, @message).deliver_now
+   ActionMailer::Base.mail(from: @email,
     to: 'your-email@example.com',
     subject: "A new contact form message from #{@name}",
     body: @message).deliver_now
-  end
+ end
 end
