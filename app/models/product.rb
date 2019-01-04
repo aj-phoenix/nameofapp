2c0ecb1e94818dc92fb5416c95d942671ea6401a
A.J. Phoenix <ajphoenix8@gmail.com>
2019-01-04 00:33:31 -0500

    5.8.5 bug fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,15 @@
 class Product < ApplicationRecord
   has_many :orders
   has_many :comments
-end
+	def self.search(search_term)
+	  Product.where("name LIKE ?", "%#{search_term}%")
+	end
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
+	def highest_rating_comment
+	  comments.rating_desc.first
+	end
 
-def highest_rating_comment
-  comments.rating_desc.first
-end
-
-def average_rating
-  comments.average(:rating).to_f
+	def average_rating
+	  comments.average(:rating).to_f
+	end
 end
